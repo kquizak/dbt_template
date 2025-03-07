c1ec99b89183f2c8e0e55fc720de5f23cad8beab
Florin Pop <florin.pop@endava.com>
2025-03-07 14:53:00 +0200

    setup env vars from airflow vars

--- airflow/dags/dbt_run_dag.py
+++ airflow/dags/dbt_run_dag.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
+from airflow.models import Variable
 
 # Define default arguments for the DAG
 default_args = {
@@ -21,13 +22,28 @@ dag = DAG(
     schedule_interval=timedelta(days=1),  # Adjust the schedule as needed
 )
 
+#if variables defined then use those else use those defined in env.sh
+dbt_run_command = """
+if [ -z "$(echo {{ var.get('DBT_GCS_STAGE', '') }})" ]; then
+    source /app/env.sh
+else
+    export SNOWFLAKE_ACCOUNT={{ var.get('SNOWFLAKE_ACCOUNT', '') }}
+    export SNOWFLAKE_USER={{ var.get('SNOWFLAKE_USER', '') }}
+    export SNOWFLAKE_PASSWORD={{ var.get('SNOWFLAKE_PASSWORD', '') }}
+    export SNOWFLAKE_ROLE={{ var.get('SNOWFLAKE_ROLE', '') }}
+    export SNOWFLAKE_DATABASE={{ var.get('SNOWFLAKE_DATABASE', '') }}
+    export SNOWFLAKE_WAREHOUSE={{ var.get('SNOWFLAKE_WAREHOUSE', '') }}
+    export DBT_GCS_STAGE={{ var.get('DBT_GCS_STAGE', '') }}
+fi
+
+dbt run --project-dir /app/snowflake_pipe --profiles-dir /home/airflow/.dbt
+"""
 
 # Define the BashOperator to run the dbt command
 dbt_run = BashOperator(
     task_id='dbt_run',
-    bash_command='source /app/env.sh && dbt run --project-dir /app/snowflake_pipe --profiles-dir /home/airflow/.dbt',
+    bash_command=dbt_run_command,
     dag=dag,
 )
 
 # Set task dependencies (if any)
-# In this simple example, there are no dependencies
